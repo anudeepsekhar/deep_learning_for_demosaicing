cfade1d439d801e3a1fdaf5e03d45d5389066877
anudeepsekhar <anudeep.sekhar@gmail.com>
2021-05-07 04:10:58 +0530

    Added testing support

--- demosaicing_v2/train_tf.py
+++ demosaicing_v2/train_tf.py
@@ -1,7 +1,7 @@
 #%%
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import cv2
 import numpy as np
@@ -53,7 +53,7 @@ def parse_data(path, train):
     img = cv2.resize(img, (cf.img_size, cf.img_size))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_down = cv2.resize(img, (cf.img_size//2, cf.img_size//2))
-    img_down = cv2.blur(img_down, (1,1))
+    # img_down = cv2.blur(img_down, (1,1))
     cfa = remosaic(img_down)
     assert cfa.shape==img.shape
 
@@ -75,12 +75,12 @@ def load_val_data(path):
     return cfa, rgb
 
 def get_test_data():
-    for batch in train_ds.take(1):
+    for batch in val_ds.take(1):
         images, target = batch
     return images, target
 
 
-def plot_results(org_image, cfa, model = None):
+def plot_results(org_image, cfa, model = None, zoom=False, epoch = None, exp_path = None):
     if model is None:
         fig, ax = plt.subplots(1,2, figsize=(4,5))
         ax[0].imshow(org_image.numpy()[0])
@@ -92,22 +92,33 @@ def plot_results(org_image, cfa, model = None):
         psnr = calc_psnr(org_image, output)
         fig, ax = plt.subplots(1,3, figsize=(12,8))
         plt.title(f'PSNR: {psnr}')
-        ax[0].imshow(org_image.numpy()[0])
-        ax[1].imshow(cfa.numpy()[0])
-        ax[2].imshow(output[0])
+        if zoom:
+            ax[0].imshow(org_image.numpy()[0][:32,:32,:])
+            ax[1].imshow(cfa.numpy()[0][:32,:32,:])
+            ax[2].imshow(output[0][:32,:32,:])
+        else:
+            ax[0].imshow(org_image.numpy()[0])
+            ax[1].imshow(cfa.numpy()[0])
+            ax[2].imshow(output[0])
         for a in ax: a.axis('off') 
         plt.show()
+        if exp_path is not None:
+            exp_path = os.path.join(exp_path, 'images')
+            if not os.path.exists(exp_path):
+                os.makedirs(exp_path)
+            fig.savefig(os.path.join(exp_path, f'fig_{epoch}.jpg'))
+#%%
+experiment_dir = '/home/aicenter/Projects/demosaicing/deep_learning_for_demosaicing/demosaicing_v2/experiments'
+experiment_name = 'model_skip_con_const_kernel_'
+run_id = 0
+experiment_name += str(run_id)
+exp_path = os.path.join(experiment_dir, experiment_name)
+if not os.path.exists(exp_path):
+    os.makedirs(exp_path)
 
 
 
 #%%
-parser = argparse.ArgumentParser(description='Demosaicnet Trainer')
-parser.add_argument('stage', help='stage of neural network you want to train')
-parser.add_argument('resume', help='resume training from latest checkpoint')
-
-
-#%%
-
 image_paths = sorted(glob(os.path.join(cf.data_dir, '*/*.jpg')))
 print(f'Number of images in data dir: {len(image_paths)}')
 train_paths, test_paths = train_test_split(image_paths, test_size=0.2, shuffle=True)
@@ -115,7 +126,6 @@ print(f'Number of images in train: {len(train_paths)}')
 print(f'Number of images in test: {len(test_paths)}')
 
 #%%
-
 train_ds = tf.data.Dataset.from_tensor_slices(train_paths)
 train_ds = train_ds.map(load_train_data, num_parallel_calls=tf.data.AUTOTUNE)
 # train_ds = train_ds.shuffle(buffer_size=len(train_ds))
@@ -127,29 +137,34 @@ val_ds = val_ds.batch(batch_size=cf.batch_size)
 
 # %%
 i = 2
-for batch in train_ds.take(1):
+for batch in val_ds.take(1):
     images, target = batch
     print(images.shape)
 fig, ax = plt.subplots(1,2)
-ax[0].imshow(images.numpy()[i][:32,:32,:])
-ax[1].imshow(target.numpy()[i][:32,:32,:])
+ax[0].imshow(images.numpy()[i])
+ax[1].imshow(target.numpy()[i])
 
 
 # %%
 mb = ModelBuilder()
-model = mb.build_model_v1()
-plot_results(target, images, model)
+model = mb.build_model_v2()
+#%%
+# model.load_weights('/home/aicenter/Projects/demosaicing/deep_learning_for_demosaicing/demosaicing_v2/checkpoints_MS_v3_s1_128/cp-0015.ckpt')
+
+plot_results(target, images, model, zoom=True, epoch=00, exp_path=exp_path)
 
 # %%
-epochs = 10
+epochs = 15
 reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
                               patience=5, min_lr=0.00001)
 
-viz_cb = VizCallback(get_test_data, plot_results, cf.plot_freq)
+viz_cb = VizCallback(get_test_data, plot_results, 5)
 
 
-checkpoint_path = "checkpoints_MS_v2_s1/cp-{epoch:04d}.ckpt"
+checkpoint_path = "checkpoints_MS_v4_s1_128/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
+
+
 
 ckpt_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
     save_weights_only=True, mode='auto', save_freq='epoch',
@@ -159,35 +174,76 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
 model.compile(
     optimizer=optimizer, loss=keras.losses.mean_absolute_error, metrics=[keras.metrics.RootMeanSquaredError()]    
 )
-
-# %%
-hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds, batch_size=cf.batch_size, verbose=1, callbacks=[ckpt_callback, reduce_lr])
-
-# %%
-#train second stage
-model1 = model
-model1.trainable = False
-
-model2 = mb.build_model_v1_s2()
-# model2.summary()
-out = model2(model1.output)
-model = keras.Model([model1.input], [out])
-
 model.summary()
-# %%
-epochs = 10
-checkpoint_path = "checkpoints_MS_v2_s2/cp-{epoch:04d}.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
-
-ckpt_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
-    save_weights_only=True, mode='auto', save_freq='epoch',
-    options=None
-    )
-
-model.compile(
-    optimizer=optimizer, loss=keras.losses.mean_squared_error, metrics=[keras.metrics.RootMeanSquaredError()]    
-)
 
 # %%
 hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds, batch_size=cf.batch_size, verbose=1, callbacks=[ckpt_callback, reduce_lr])
+#%%
+hist_df = pd.DataFrame(hist.history)
+hist_df.head()
+hist_df.to_csv(os.path.join(exp_path, f'history_run_{4}.csv'))
+
+#%%
+plot_results(target, images, model, epoch=15, exp_path=exp_path)
+
+#%%
+tot_psnr = 0
+c = 1
+model.load_weights('/home/aicenter/Projects/demosaicing/deep_learning_for_demosaicing/demosaicing_v2/checkpoints_MS_v3_s1_256/cp-0005.ckpt')
+for image, target in tqdm(val_ds):
+    output = model(image)
+    # plot_results(target, images, model)
+    # break
+    for i in range(len(image)):
+        psnr = calc_psnr(target[i], output[i])
+        tot_psnr += psnr
+        c += 1
+#     break
+# fig, ax = plt.subplots(1,2)
+# ax[0].imshow(output.numpy()[i])
+# ax[1].imshow(target.numpy()[i])
+# # break
+#%%
+avg_psnr = tot_psnr/c
+print(f'Avg PSNR: {avg_psnr}')
+        
+
+
+
+
+
+
+
+
+
+
+# # %%
+# #train second stage
+# model1 = model
+# model1.trainable = False
+
+# model2 = mb.build_model_v1_s2()
+# # model2.summary()
+# out = model2(model1.output)
+# model = keras.Model([model1.input], [out])
+
+# model.summary()
+# # %%
+# epochs = 10
+# checkpoint_path = "checkpoints_MS_v2_s2/cp-{epoch:04d}.ckpt"
+# checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# ckpt_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
+#     save_weights_only=True, mode='auto', save_freq='epoch',
+#     options=None
+#     )
+
+# model.compile(
+#     optimizer=optimizer, loss=keras.losses.mean_squared_error, metrics=[keras.metrics.RootMeanSquaredError()]    
+# )
+
+# # %%
+# hist = model.fit(train_ds, epochs=epochs, validation_data=val_ds, batch_size=cf.batch_size, verbose=1, callbacks=[ckpt_callback, reduce_lr])
+# # %%
+
 # %%
