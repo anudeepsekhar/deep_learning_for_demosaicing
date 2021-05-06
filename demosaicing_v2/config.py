218f7d176face9c46b707551ef5cb5916f7c5bee
anudeepsekhar <anudeep.sekhar@gmail.com>
2021-05-07 04:10:24 +0530

    Added new config

--- demosaicing_v2/config.py
+++ demosaicing_v2/config.py
@@ -2,7 +2,7 @@ class Config():
     def __init__(self):
         self.img_size = 128
         self.n_channels = 3
-        self.input_shape = [self.img_size,self.img_size, self.n_channels]
+        self.input_shape = [None,None,3]
         self.data_dir = '/home/aicenter/Projects/demosaicing/deep_learning_for_demosaicing/data/CUB_200_2011/images'
-        self.batch_size = 32
+        self.batch_size = 16
         self.plot_freq = 2
